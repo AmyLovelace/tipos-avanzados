b421fe83136118deaec12321c38e3f17de76c742
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-11 21:40:36 -0300

    added a list exercise

---
+++ 01-listas.py
@@ -0,0 +1,10 @@
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+palabras = ["chanchito", "feliz"]
+palabrasfelices = ["ami", "feliz", "chupicio", " bichicio"]
+matriz = [[0,1], [1,0]]
+ceros = [0] * 10
+alfanumerico = numeros + letras
+rango = list(range(1, 11))
+chars = list("hola mundo")
+print(chars)
