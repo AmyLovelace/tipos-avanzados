b8607ba4e8c1fcbe0a6a249a2814acd652d8bae5
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-11 22:15:17 -0300

    added a list exercise

--- 02-manipulando-listas.py
+++ 02-manipulando-listas.py
@@ -1,1 +1,16 @@
 #manipulando listas 
+mascotas = ["pulgas", "pelusa", "copito", "mechi", "pancito"]
+print(mascotas[2])
+mascotas[2] = "molotov"
+#print(mascotas)
+#print(mascotas[2:])#AQUI SE UTILIZA EL : PARA LLAMAR DE UNA PARTE DE 
+#LA LISTA A OTRA EN ESTE CASO SERIA DESDE EL STRING 2 AL X
+#print(mascotas[-3])
+#AQUI TE DEVUELVES Y TE LLAMA EL STRING QUE ESTE EN ESE LUGAR EN ESTE CASO ES MOLOTOV 
+#print(mascotas[1::2])
+#con el ::te dice salte uno los valores expresan de donde a donde , en este caso vas desde 
+#pelusa y mechi saltanto a copito  
+
+numeros = list(range(21))
+print(numeros[::2])
+print(numeros[1::2])
---
+++ 03-desempaquetar-listas.py
@@ -0,0 +1,5 @@
+numeros = [1, 2, 3]
+#feo
+#primero = numeros[0]
+#primero = numeros[1]
+#primero = numeros[2]
