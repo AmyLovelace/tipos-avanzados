d2dc7fffcb30a9264a966256698239f31b245c5b
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-12 14:43:49 -0300

    added filter map list exercise

--- 08-comprension-listas.py
+++ 08-comprension-listas.py
@@ -2,3 +2,23 @@ usuarios = [["ami", 4],
             ["copin", 2 ],
             ["andy", 10]
 ]
+
+#nombres = []
+#for usuario in usuarios:
+#    nombres.append(usuario[0])
+#print(nombres)
+
+#map
+#nombres = [usuario[0] for usuario in usuarios]#aqui le indicas que quieres llamar al indice 0 de cada usuario , osea su nombre
+
+#filtrar /filter
+#nombres = [usuario for usuario in usuarios if usuario[1]> 2]#asi puedes filtrar elementos dentro de la lista
+#nombres = [usuario[0] for usuario in usuarios if usuario[1]> 2]#asi puedes transformar y filtrar la lista
+
+
+
+#nombres = list(map(lambda usuario: usuario [], usuarios))
+
+menosUsuarios = list(filter(lambda usuario: usuario[1] > 2, usuarios))
+
+print(menosUsuarios)
