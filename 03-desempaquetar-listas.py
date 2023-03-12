40d8814debd99819b07840fdbfa7559d1110fe2a
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-11 22:29:46 -0300

    added a list exercise

--- 03-desempaquetar-listas.py
+++ 03-desempaquetar-listas.py
@@ -1,5 +1,9 @@
-numeros = [1, 2, 3]
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 #feo
 #primero = numeros[0]
 #primero = numeros[1]
-#primero = numeros[2]
+#primero = numeros[2] 
+
+#lindo
+primero, *otros, ultimo  = numeros # *esto para solo llamar a un numero de la lista
+print(primero, ultimo, otros)
