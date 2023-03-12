19b4dbef50fd682f034fda8eede1f253a7a653be
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-12 13:08:51 -0300

    added list exercise insert append remove pop clear

---
+++ 06-agregando-eliminando.py
@@ -0,0 +1,17 @@
+mascotas = [
+    "pelusa",         
+    "copin", 
+    "molotov",
+    "pancito",
+    "molotov",
+    "mechi"
+]
+mascotas.insert(1, "pancito")#1 es el indice de donde insertare el nuevo objeto
+mascotas.append("michi")
+
+mascotas.remove("molotov")
+mascotas.pop(1)#para eliminar desde el indice del elemento
+del mascotas[4]#para eliminar desde el indice del elemento
+mascotas.clear()#para eliminar la lista completa
+print(mascotas)
+
