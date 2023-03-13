1e90bdfcd7430c5ec336281476f66a2193850836
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-13 11:03:25 -0300

    added a unpacking exercise

--- 12-operador-desempaquetar.py
+++ 12-operador-desempaquetar.py
@@ -1,1 +1,27 @@
 #operador desempaquetar
+
+lista1 = [1, 2, 3, 4]
+#print(*lista1)
+
+lista2=[5,6]
+
+combinada =["holis", *lista1, "bish",*lista2]
+
+print(combinada)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
